eca4b897eea55ca55a7a42dea0d8f3b6e7d75d79
unknown <13126668274@163.com>
2020-08-10 17:46:53 +0800

    创建

---
+++ TestSeleniumMode/src/test/java/fan/selenium/testMode/util/Constant.java
@@ -0,0 +1,12 @@
+package fan.selenium.testMode.util;
+//定义常量
+public class Constant {
+	//定义Excel测试数据文件的路径
+	public static final String TestDataExcelFilePath = "E:/Projects/TestSeleniumMode/Excel/TestExcelCase.xlsx";
+	//定义Cookie文件位置
+	public static final String TestDataCookieFilePath = "../TestSeleniumMode/src/test/resources/cookie.properties";
+	//定义浏览器启动驱动名称
+	public static final String TestDatachromedriverPath = "../TestSeleniumMode/driver/chromedriver.exe";
+	public static final String TestDatageckodiverPath = "../TestSeleniumMode/driver/geckodiver.exe";
+	public static final String TestDataoperadriverPath = "../TestSeleniumMode/driver/operadriver.exe";
+}
